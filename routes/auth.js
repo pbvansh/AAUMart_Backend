700d2f3c5eedcc94c14e2d1c87e34af7a53158f5
vidhi <vanshpratik0165@gmail.com>
2022-09-19 16:50:52 +0530

    solve error

--- routes/auth.js
+++ routes/auth.js
@@ -4,37 +4,36 @@ const User = require('../model/userModel')
 const BC = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 
-route.post('/login', asyncHandler(async(req, res) => {
+route.post('/login', asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    if(!email || !password){
-        res.status(400).json({msg : "Please provide email or password"})
+    if (!email || !password) {
+        res.status(400).json({ msg: "Please provide email or password" })
+    } else {
+        const user = await User.findOne({ email })
+        if (user && BC.compare(password, user.password)) {
+
+            res.status(200).json({
+                auth: true,
+                token: user.createJWT()
+            })
+        } else {
+            res.status(400).json({ msg: 'invalid email or password' })
+        }
     }
 
-    const user = await User.findOne({email})
-    if(user && BC.compare(password,user.password)){
-
-        res.status(200).json({
-            auth : true,
-            token : user.createJWT()
-        })
-    }else{
-        res.status(400).json({msg : 'invalid email or password'})
-    }
-
-    res.status(200).json('login')
 }))
 
 
 //POST signup user
-route.post('/signup', asyncHandler(async(req, res) =>{
+route.post('/signup', asyncHandler(async (req, res) => {
 
-    const  { name ,email , mobilenumber , password , isAdmin} = req.body;
+    const { name, email, mobilenumber, password, isAdmin } = req.body;
 
-    if(!email || !password) {
+    if (!email || !password) {
         res.status(400).json("please enter valid email or password")
     }
 
-    const user = await User.create({name,email,mobilenumber,password,isAdmin})
+    const user = await User.create({ name, email, mobilenumber, password, isAdmin })
     res.status(200).json(user)
 }))
 
