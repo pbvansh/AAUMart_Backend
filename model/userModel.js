a28838c40a00b267bce24902ea68b254db927a07
vidhi <vanshpratik0165@gmail.com>
2022-09-20 15:17:07 +0530

    add bcriptjs

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const JWT = require('jsonwebtoken')
 require('dotenv').config()
+const bc = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -23,6 +24,13 @@ const userSchema = new mongoose.Schema({
         default : false,
     }
 })
+
+
+userSchema.pre('save', async function(){
+    const salt = await bc.genSalt(10)
+    this.password = await bc.hash(this.password,salt)
+})
+
 
 userSchema.methods.createJWT = function(){
     return JWT.sign({userId :this._id , userEmail : this.email},process.env.JWTSECRET,{
