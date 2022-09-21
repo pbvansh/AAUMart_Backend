a8182aa7f171a2798ba3da7d48cc1c7c8d0ea184
vidhi <vanshpratik0165@gmail.com>
2022-09-21 14:53:29 +0530

    handel unique email error

--- routes/auth.js
+++ routes/auth.js
@@ -31,13 +31,17 @@ route.post('/login', asyncHandler(async (req, res) => {
 route.post('/signup', asyncHandler(async (req, res) => {
 
     const { name, email, mobilenumber, password, isAdmin } = req.body;
-
-    if (!email || !password) {
-        res.status(400).json("please enter valid email or password")
+    try {
+        if (!email || !password) {
+            res.status(400).json("please enter valid email or password")
+        }
+    
+        const user = await User.create({ name, email, mobilenumber, password, isAdmin })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({msg : error.code && ('Email address is already exist')})
     }
-
-    const user = await User.create({ name, email, mobilenumber, password, isAdmin })
-    res.status(200).json(user)
+    
 }))
 
 // verify JWT 
