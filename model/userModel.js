0540241d6b1a25feb5315b3302af8b72a4a00af3
vidhi <vanshpratik0165@gmail.com>
2022-09-21 14:37:25 +0530

    make eamil unique

--- model/userModel.js
+++ model/userModel.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
     },
     email : {
         type:String,
+        unique : true,
         required : [true , 'please enter your email']
     },
     mobilenumber : {
