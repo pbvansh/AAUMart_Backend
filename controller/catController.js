489468df3faa71911da903777cf63f1483064dd2
vidhi <vanshpratik0165@gmail.com>
2022-10-06 14:41:09 +0530

    add route for delete catogory

--- controller/catController.js
+++ controller/catController.js
@@ -6,12 +6,19 @@ const getAllCat = asyncHandler(async (req, res) => {
     res.status(200).json(cat)
 })
 
-const addCategory = asyncHandler(async(req,res)=>{
+const addCategory = asyncHandler(async (req, res) => {
     const cat = await Category.create(req.body)
     res.status(200).json(cat)
+})
+
+const deleteCategory = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    await Category.findByIdAndDelete(id)
+    res.status(200).json({ msg: 'Catogory deleted successful' })
 })
 
 module.exports = {
     getAllCat,
     addCategory,
+    deleteCategory,
 }
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -3,9 +3,11 @@ const route = express.Router()
 const {
     getAllCat,
     addCategory,
+    deleteCategory
 } = require('../controller/catController')
 
 route.get('/',getAllCat)
 route.post('/addNew',addCategory)
+route.delete('/:id',deleteCategory)
 
 module.exports = route
