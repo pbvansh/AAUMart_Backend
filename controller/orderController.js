ba55fb3c8fb277cd7fd6498103c06683945b346e
vidhi <vanshpratik0165@gmail.com>
2022-10-13 16:16:45 +0530

    remove stripe and add rezorpay

--- controller/orderController.js
+++ controller/orderController.js
@@ -38,6 +38,14 @@ const placeOrder = asyncHandler(async (req, res) => {
 
 })
 
+
+const paymentVerification = asyncHandler(async(req,rs)=>{
+    console.log(req.body);
+    res.status(200).json({success : true})
+
+})
+
 module.exports = {
     placeOrder,
+    paymentVerification
 }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 const route = express.Router();
 const {
     placeOrder,
+    paymentVerification
 } = require('../controller/orderController');
 const { protect } = require('../middleware/protect');
 
 route.post('/placeOrder',protect, placeOrder);
+route.post('payment',protect,paymentVerification)
 
 module.exports = route;
