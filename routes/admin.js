0d0ff2d8d6552e700d31b2fdda5249efe7d0847f
vidhi <vanshpratik0165@gmail.com>
2022-11-03 16:25:03 +0530

    create route for upadate status of order

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,20 @@ const { protect } = require('../middleware/protect');
 const route = express.Router();
 const Order_item = require('../model/order_itemsModel')
 
-route.get('/orders', protect, async(req, res) => {
+route.get('/orders', protect, async (req, res) => {
     try {
         const orders = await Order_item.find().populate('products.id')
         res.status(200).json(orders)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+route.put('/order/:id', protect, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const order = await Order_item.findByIdAndUpdate(id, req.body, { new: true })
+        res.status(200).json(order)
     } catch (error) {
         res.status(500).send(error)
     }
