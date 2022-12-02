176bedaacf931418fdbdc7c3ee31160531cb299a
pbvansh <vanshpratik0165@gmail.com>
2022-12-02 11:02:30 +0530

    solve error

--- config/db.js
+++ config/db.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async()=>{
     try {
         const URL = process.env.MONGO_URI;
-        const conn = await mongoose.connect(URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            family: 4,
-          })
+        const conn = await mongoose.connect(URL)
         console.log(conn.connection.host);
     } catch (err) {
         console.log(err);
