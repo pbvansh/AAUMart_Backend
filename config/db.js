9d8a7f9cb866cd9257184c46ff4e302e37a4894b
pbvansh <vanshpratik0165@gmail.com>
2022-12-02 15:48:23 +0530

    try

--- config/db.js
+++ config/db.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const connectDB = async()=>{
     try {
         const URL = process.env.MONGO_URI;
-        const conn = await mongoose.connect(URL)
-        console.log(conn.connection.host);
+        const conn = mongoose.connect(URL).then((conn)=>{
+            console.log(conn.connection.host);
+        })
+        // console.log(conn.connection.host);
     } catch (err) {
         console.log(err);
     }
