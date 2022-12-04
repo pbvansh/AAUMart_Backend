dc9f0699ef49aa7f37eba073e2f8c32c5d040423
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-04 11:07:42 +0000

    add product filter

--- controller/productController.js
+++ controller/productController.js
@@ -2,48 +2,61 @@ const asyncHandler = require('express-async-handler')
 const Product = require('../model/productModel')
 
 const getAllProducts = asyncHandler(async (req, res) => {
-    console.log(req.query);
-    const { Min, Max } = req.query;
-    const products = await Product.find();
-    res.status(200).json(products)
+  let queries = [];
+  const { Min, Max, category, name } = req.query;
+
+  if (category != undefined) {
+    queries.push({ category })
+  } else queries.push({})
+
+  if (name !== undefined) {
+    queries.push({ name })
+  } else queries.push({})
+  
+  if (Min !== undefined && Max !== undefined) {
+    queries.push({ "price": { $gt: Number(Min), $lt: Number(Max) } })
+  }
+
+  const products = await Product.find({ "$or": queries });
+  res.status(200).json(products)
 })
 
 const getStaticProduct = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const product = await Product.findById(id);
-    res.status(200).json(product)
+  const id = req.params.id;
+  const product = await Product.findById(id);
+  res.status(200).json(product)
 })
 
 const addProduct = asyncHandler(async (req, res) => {
-    const { name, desc, category, price } = req.body;
-    const product = await Product.create({ name, desc, category, price })
-    console.log('addProduct');
-    res.status(200).json(product)
+  const { name, desc, category, price } = req.body;
+  const product = await Product.create({ name, desc, category, price })
+  console.log('addProduct');
+  res.status(200).json(product)
 })
 
 const addImgUrl = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
-    res.status(200).json(product)
+  const id = req.params.id;
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
+  res.status(200).json(product)
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
-    res.status(200).json(product);
+  const id = req.params.id;
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
+  res.status(200).json(product);
 })
 
 const deleteProduct = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    await Product.findByIdAndDelete(id)
-    res.status(200).json({ msg: "prodicu deleted successfuly" });
+  const id = req.params.id;
+  await Product.findByIdAndDelete(id)
+  res.status(200).json({ msg: "prodicu deleted successfuly" });
 })
 
 module.exports = {
-    getAllProducts,
-    getStaticProduct,
-    addProduct,
-    addImgUrl,
-    updateProduct,
-    deleteProduct
+  getAllProducts,
+  getStaticProduct,
+  addProduct,
+  addImgUrl,
+  updateProduct,
+  deleteProduct
 }
