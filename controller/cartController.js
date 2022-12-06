1f488887d0656de39d36a119c61f1666793fdc48
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-06 10:55:34 +0000

    add route for total item in cart

--- controller/cartController.js
+++ controller/cartController.js
@@ -3,37 +3,45 @@ const Cart = require('../model/cartModel')
 
 const addProductToCart = asyncHandler(async (req, res) => {
 
-    const item = await Cart.create(req.body)
-    res.status(200).json(item)
+  const item = await Cart.create(req.body)
+  res.status(200).json(item)
 
 })
 
+const getTotalNoOfItem = asyncHandler(async (req, res) => {
+  const user_id = req.params.id;
+  const item = await Cart.countDocuments({ user_id, isOrdered: false })
+  res.status(200).json(item)
+})
+
+
 const removeProductToCart = asyncHandler(async (req, res) => {
 
-    const id = req.params.id;
-    const item = await Cart.findByIdAndDelete(id)
-    res.status(200).json(item)
+  const id = req.params.id;
+  const item = await Cart.findByIdAndDelete(id)
+  res.status(200).json(item)
 
 })
 
 const updateProductToCart = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const item = await Cart.findByIdAndUpdate(id, req.body, { new: true })
-    res.status(200).json(item)
+  const id = req.params.id;
+  const item = await Cart.findByIdAndUpdate(id, req.body, { new: true })
+  res.status(200).json(item)
 
 })
 
 
 const getItemToCart = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const item = await Cart.find({ user_id: id , isOrdered :false }).populate('product_id')
-    res.status(200).json(item)
+  const id = req.params.id;
+  const item = await Cart.find({ user_id: id, isOrdered: false }).populate('product_id')
+  res.status(200).json(item)
 
 })
 
 module.exports = {
-    addProductToCart,
-    removeProductToCart,
-    updateProductToCart,
-    getItemToCart,
+  addProductToCart,
+  removeProductToCart,
+  updateProductToCart,
+  getItemToCart,
+  getTotalNoOfItem
 }
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const route = express.Router()
 const {
-    addProductToCart,
-    removeProductToCart,
-    updateProductToCart,
-    getItemToCart
+  addProductToCart,
+  removeProductToCart,
+  updateProductToCart,
+  getItemToCart,
+  getTotalNoOfItem
 } = require('../controller/cartController')
 
-route.post('/addItem',addProductToCart);
-route.delete('/:id',removeProductToCart);
-route.put('/:id',updateProductToCart);
-route.get('/:id',getItemToCart);
+route.post('/addItem', addProductToCart);
+route.get('/getTotal/:id', getTotalNoOfItem)
+route.delete('/:id', removeProductToCart);
+route.put('/:id', updateProductToCart);
+route.get('/:id', getItemToCart);
 
 module.exports = route;
