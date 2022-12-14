4ca17672a966b41be56b6e728d9545f28226536d
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-14 12:30:55 +0000

    add

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,7 @@ const User = require("../model/userModel")
 const Address = require('../model/addressModel')
 
 const getAllUser = async (req, res) => {
-  const user = await User.find()
+  const user = await User.find().select('-password')
   res.status(200).json(user)
 }
 
@@ -11,12 +11,19 @@ const getAddress = async (req, res) => {
     const address = await Address.countDocuments({ user_id: req.user.userId })
     res.status(200).json(address)
   }
-  catch(e)  {
+  catch (e) {
     res.status(400).json('error')
   }
 }
 
+const deleteUser = async (req, res) => {
+  const _id = req.params.id;
+  const { firstName } = await User.findByIdAndDelete({ _id });
+  res.status(200).json({ msg: `${firstName ? firstName + 'account deleted successfully' : 'account deleted successfully'}` })
+}
+
 module.exports = {
   getAllUser,
-  getAddress
+  getAddress,
+  deleteUser
 }
