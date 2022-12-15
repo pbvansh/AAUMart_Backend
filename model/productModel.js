f844809450f926bd9e11e9d93d23ac2142bce34b
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-15 19:33:28 +0000

    add stock

--- model/productModel.js
+++ model/productModel.js
@@ -2,33 +2,37 @@ const mongoose = require('mongoose')
 
 const prodSchema = new mongoose.Schema({
 
-    name:{
-        type:String,
-        required : [true,'please provide product name'],
-        min:1,
-        max:50,
-        unique : true,
-    },      
-    desc:{
-        type: String,
-        required : [true,'please provide product name'],
-        min:1,
-        max:1000,
-    },
-    category : {
-        type : String,
-        required :[true,'Please provide product category']
-    },
-    price : {
-        type : Number,
-        required : [true,'please provide product price'],
-    },
-    img_url :{
-         type : String,
-    }
+  name: {
+    type: String,
+    required: [true, 'please provide product name'],
+    min: 1,
+    max: 50,
+    unique: true,
+  },
+  desc: {
+    type: String,
+    required: [true, 'please provide product name'],
+    min: 1,
+    max: 1000,
+  },
+  category: {
+    type: String,
+    required: [true, 'Please provide product category']
+  },
+  price: {
+    type: Number,
+    required: [true, 'please provide product price'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'please provide product stock'],
+  },
+  img_url: {
+    type: String,
+  }
 },
-{
-    timestamps:true
-})
+  {
+    timestamps: true
+  })
 
-module.exports = mongoose.model('Product',prodSchema)
+module.exports = mongoose.model('Product', prodSchema)
