0d6d7c12fbaf3cbcdb5b862a3e5962ea52e78d92
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-16 17:50:46 +0000

    send email

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,55 @@
 const User = require("../model/userModel")
+// const fetch = require('node-fetch')
 const Address = require('../model/addressModel')
+import fetch from "node-fetch"
+
+
+
 
 const getAllUser = async (req, res) => {
   const user = await User.find().select('-password')
   res.status(200).json(user)
 }
+
+const resetPassword = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.find({ email })
+
+  if (user) {
+    const OTP = Math.floor(Math.random() * 1000000);
+    console.log(OTP)
+    var data = {
+      service_id: process.env.YOUR_SERVICE_ID,
+      template_id: process.env.YOUR_TEMPLATE_ID,
+      user_id: process.env.YOUR_PUBLIC_KEY,
+      template_params: {
+        'to_name': email,
+        'message': OTP
+      }
+    }
+    console.log(data)
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json'
+    }).then(function() {
+      res.status(200).json({ msg: 'We send you OPT please check you email', OTP })
+    }).catch(function(error) {
+      res.status(200).json({ msg: 'Oops... ' + error });
+    });
+  } else {
+    res.status(200).json({ msg: 'invalid email' })
+  }
+}
+
+
+
+
+
+
+
+
+
 
 const getAddress = async (req, res) => {
   try {
@@ -25,5 +70,6 @@ const deleteUser = async (req, res) => {
 module.exports = {
   getAllUser,
   getAddress,
-  deleteUser
+  deleteUser,
+  resetPassword
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,13 @@ const route = express.Router()
 const { protect } = require('../middleware/protect');
 const { getAllUser,
   getAddress,
-  deleteUser
+  deleteUser,
+  resetPassword
 } = require('../controller/userController')
 
 route.get('/', getAllUser)
 route.get('/address', protect, getAddress)
 route.delete('/:id', protect, deleteUser)
+route.post('/reset', resetPassword)
 
 module.exports = route
