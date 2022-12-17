46d0276e77414278f63346b47d3ee7f8d05aef05
pbvansh <vanshpratik0165@gmail.com>
2022-12-17 09:21:02 +0530

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,10 @@
 const User = require("../model/userModel")
 const Address = require('../model/addressModel')
+const SMTPClient = require('emailjs')
 
-<<<<<<< HEAD
-const  getAllUser = async(req,res) =>{
-    const user = await User.find()
-    res.status(200).json(user)
-=======
 const getAllUser = async (req, res) => {
   const user = await User.find().select('-password')
   res.status(200).json(user)
->>>>>>> b219b3104a529ee2f8cd0d1c3d98113dcf87ef91
 }
 
 const getAddress = async (req, res) => {
@@ -28,8 +23,15 @@ const deleteUser = async (req, res) => {
   res.status(200).json({ msg: `${firstName ? firstName + 'account deleted successfully' : 'account deleted successfully'}` })
 }
 
+const resetPassword = async (req, res) => {
+  const OPT = Math.random() * 100000;
+  console.log(OPT);
+}
+
+
 module.exports = {
   getAllUser,
   getAddress,
-  deleteUser
+  deleteUser,
+  resetPassword
 }
